cc4c7b3400dc1ad9a5b39f0245577d9965db40d9
Tan Jia Jia <71870881+jiajiatan@users.noreply.github.com>
2022-02-16 14:03:05 +0800

    added & update confirmation modals

--- LivingLab.Web/wwwroot/js/confirmationmodals.js
+++ LivingLab.Web/wwwroot/js/confirmationmodals.js
@@ -8,27 +8,70 @@ function showAlert() {
         },
         inputPlaceholder: 'Upload Type',
         showCancelButton: true,
+        reverseButtons: true,
         confirmButtonColor: '#312E81',
         cancelButtonColor: '#C0C0C0',
         confirmButtonText: 'Submit',
-        inputValidator: function (value) {
-            return new Promise(function (resolve, reject) {
-                if (value === 'uploadfiles') {
-                    //do something
-                    resolve()
-                } else {
-                    // do something
-                    resolve()
+    }).then(function (result) {
+        if (result.value === "uploadfiles") {
+            window.location.href = "ManualLogs/FileUpload";
+        } else if (result.value === "addlogs") {
+            window.location.href = "ManualLogs/ManualLogUpload";
+        }
+    })
+}
+
+function addLogAlert() {
+    Swal.fire({
+        title: 'Do you want to add this log?',
+        icon: 'warning',
+        showCancelButton: true,
+        reverseButtons: true,
+        confirmButtonColor: '#312E81',
+        cancelButtonColor: '#C0C0C0',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Log Added',
+                icon: 'success',
+                showDenyButton: true,
+                reverseButtons: true,
+                confirmButtonColor: '#312E81',
+                denyButtonColor: '#C0C0C0',
+                confirmButtonText: 'Add New Logs',
+                denyButtonText: 'Back to Dashboard'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    /*redirects to add new logs page (subject to changes)*/
+                    window.location.href = "ManualLogs/ManualLogUpload";
+                } else if (result.isDenied) {
+                    /*redirects to dashboard page (subject to changes)*/
+                    window.location.href = "/";
                 }
             })
         }
-    }).then(function (result) {
-        if (result.value === "uploadfiles") {
-            console.log(window.url);
-            window.location.href = "ManualLogs/FileUpload";
-        } else if (result.value === "addlogs") {
-            console.log(window.url);
+    })
+}
+
+function fileUploadedAlert() {
+    Swal.fire({
+        title: 'File Uploaded',
+        icon: 'success',
+        showDenyButton: true,
+        reverseButtons: true,
+        confirmButtonColor: '#312E81',
+        denyButtonColor: '#C0C0C0',
+        confirmButtonText: 'My Logs',
+        denyButtonText: 'Back to Dashboard'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            /*redirects to view my logs page (subject to changes)*/
             window.location.href = "ManualLogs/ManualLogUpload";
+        } else if (result.isDenied) {
+            /*redirects to dashboard page (subject to changes)*/
+            window.location.href = "/";
         }
     })
 }
