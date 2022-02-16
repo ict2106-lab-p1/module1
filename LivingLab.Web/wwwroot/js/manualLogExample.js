446ab4b98e9ad07b3609523a45b9226fdee5ec8a
Ng Han Yi <nghanyi1997@gmail.com>
2022-02-16 16:04:31 +0800

    Implement adding and saving functionality

--- LivingLab.Web/wwwroot/js/manualLogExample.js
+++ LivingLab.Web/wwwroot/js/manualLogExample.js
@@ -1,16 +1,41 @@
+const template = `<tr>
+                    <td>{index}</td>
+                    <td class="serial-no text-center">{deviceSerialNumber}</td>
+                    <td><input class="energy shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
+                    <td><input class="interval shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
+                    <td><input class="logged-date shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value=""/></td>
+                    <td><button type="button" class="btn btn-close bg-red-500" data-index="{index}">Delete</button></td>
+                </tr>`;
+
+
 $(document).ready(function (){
     $("#btnAdd").click(appendInputRow);
+    $("#btnSave").click(save);
 })
 
-const template = `<tr>
-                    <td class="text-center">{deviceSerialNumber}</td>
-                    <td><input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
-                    <td><input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
-                    <td><input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
-                    <td><button class="btn btn-close bg-red-500">Delete</button></td>
-                </tr>`;
 
 function appendInputRow() {
     const deviceSerialNumber = $("#deviceSerialNumber").val();
-    $("#logTable tr:last").after(template.replace("{deviceSerialNumber}", deviceSerialNumber));
+    const index = $("#logTable tbody tr").length + 1;
+    $("#logTable tr:last").after(template.replaceAll("{index}", index).replace("{deviceSerialNumber}", deviceSerialNumber));
+}
+
+function save(e) {
+    e.preventDefault();
+    const $table = $("#logTable");
+    const $rows = $table.find("tbody > tr");
+    let data = [];
+    $rows.each(function () {
+        const serialNumber = $(this).find("td.serial-no").text();
+        const energyUsage = $(this).find("td > input.energy").val();
+        const interval = $(this).find("td > input.interval").val();
+        const loggedAt = $(this).find("td > input.logged-date").val();
+        data.push({
+            serialNumber,
+            energyUsage,
+            interval,
+            loggedAt
+        })
+    })
+    console.log(data)
 }
