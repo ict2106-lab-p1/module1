3944604a54b20cebb65225efe3541e647a946e27
Ng Han Yi <nghanyi1997@gmail.com>
2022-02-16 16:58:23 +0800

    Rename js files

--- LivingLab.Web/wwwroot/js/manualLogExample.js
+++ LivingLab.Web/wwwroot/js/manualLogExample.js
@@ -1,35 +1,51 @@
 const template = `<tr>
                     <td>{index}</td>
                     <td class="serial-no text-center">{deviceSerialNumber}</td>
-                    <td><input class="energy shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
-                    <td><input class="interval shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value=""/></td>
-                    <td><input class="logged-date shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value=""/></td>
-                    <td><button type="button" class="btn btn-close bg-red-500" data-index="{index}">Delete</button></td>
+                    <td><input class="energy shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value="" required/></td>
+                    <td><input class="interval shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" value="" required/></td>
+                    <td><input class="logged-date shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value="" required/></td>
+                    <td><button type="button" class="delete btn btn-close bg-red-500" data-index="{index}">Delete</button></td>
                 </tr>`;
 
-
 $(document).ready(function (){
-    $("#btnAdd").click(appendInputRow);
+    $("#btnAdd").click(appendRow);
+    $(this).on('click', '.delete', deleteRow);
     $("#btnSave").click(save);
+    
 })
 
-
-function appendInputRow() {
+function appendRow() {
     const deviceSerialNumber = $("#deviceSerialNumber").val();
     const index = $("#logTable tbody tr").length + 1;
     $("#logTable tr:last").after(template.replaceAll("{index}", index).replace("{deviceSerialNumber}", deviceSerialNumber));
 }
 
+function deleteRow() {
+    // Remove row from table
+    $(this).closest("tr").remove();
+    
+    // Update numbers
+    $("#logTable tbody tr").each(function(i) {
+        $(this).find("td:first").text(i + 1);
+    })
+}
+
 function save(e) {
     e.preventDefault();
+    const data = getData();
+    console.log(data)
+}
+
+function getData() {
+    let data = [];
     const $table = $("#logTable");
     const $rows = $table.find("tbody > tr");
-    let data = [];
     $rows.each(function () {
         const serialNumber = $(this).find("td.serial-no").text();
         const energyUsage = $(this).find("td > input.energy").val();
         const interval = $(this).find("td > input.interval").val();
         const loggedAt = $(this).find("td > input.logged-date").val();
+
         data.push({
             serialNumber,
             energyUsage,
@@ -37,5 +53,5 @@ function save(e) {
             loggedAt
         })
     })
-    console.log(data)
+    return data;
 }
