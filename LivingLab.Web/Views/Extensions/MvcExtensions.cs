f625747d3447a370f67c2d8f3ff7d4fde0620061
Ng Han Yi <nghanyi1997@gmail.com>
2022-02-21 20:33:19 +0800

    #101 set navbar active and add icons

--- LivingLab.Web/Views/Extensions/MvcExtensions.cs
+++ LivingLab.Web/Views/Extensions/MvcExtensions.cs
@@ -4,14 +4,14 @@ namespace LivingLab.Web.Views.Extensions;
 
 public static class MvcExtensions
 {
-    public static string ActiveClass(this IHtmlHelper htmlHelper, string controllers, string actions, string cssClass = "bg-highlight")
+    public static string ActiveClass(this IHtmlHelper htmlHelper, string controllers, string actions, string cssClass = "text-gray-700 bg-highlight")
     {
         var routeData = htmlHelper.ViewContext.RouteData;
 
         var routeAction = routeData.Values["action"].ToString();
         var routeController = routeData.Values["controller"].ToString();
 
-        var returnActive = (routeController == controllers && routeAction == actions) ? cssClass : "";
+        var returnActive = (routeController == controllers && routeAction == actions) ? cssClass : "text-gray-400";
 
         return returnActive;
     }
