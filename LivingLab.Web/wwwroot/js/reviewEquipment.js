17e8258cadf7dcda652c2eb6cbe3b76ea1cc1fba
angelenejoshna <44151051+angelenejoshna@users.noreply.github.com>
2022-03-31 01:40:05 +0800

    Device/Accessory Modal UI fixes

--- LivingLab.Web/wwwroot/js/reviewEquipment.js
+++ LivingLab.Web/wwwroot/js/reviewEquipment.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
                 // defaultContent: '<button class=\'hover:bg-red-300 font-large rounded-lg text-sm px-5 py-2.5 \'><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">\n' +
                 //     '  <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />\n' +
                 //     "</svg></button>",
-                "defaultContent": "<button type='button' class='deviceItem text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>Review</button>"
+                "defaultContent": "<button type='button' class='deviceItem btn btn-primary font-bold rounded mr-2'>Review</button>"
             },
         ],
         bInfo: false,
@@ -52,7 +52,7 @@ $(document).ready(function () {
                 // defaultContent: '<button class=\'hover:bg-red-300 font-large rounded-lg text-sm px-5 py-2.5 \'><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">\n' +
                 //     '  <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />\n' +
                 //     "</svg></button>",
-                "defaultContent": "<button type='button' class='accessoryItem text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>Review</button>"
+                "defaultContent": "<button type='button' class='accessoryItem btn btn-primary font-bold rounded mr-2'>Review</button>"
             },
         ],
         bInfo: false,
