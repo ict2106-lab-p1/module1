9a770b861fead521a39b62bf6f93bc3b80c72220
Yip Hou Liang <houliangyip@gmail.com>
2022-04-02 21:45:54 +0800

    Bug Fixes and implement Liskov (#192) (#193)
    * Added add buttons in type view
    Added equipment abstract class for device and accessory to inherit
    
    * add equipment abstract class
    
    Co-authored-by: Sheng Yu, Lucas <32284042+shengyu98@users.noreply.github.com>

--- LivingLab.Web/wwwroot/js/viewDeviceType.js
+++ LivingLab.Web/wwwroot/js/viewDeviceType.js
@@ -3,14 +3,15 @@
 /* </remarks>*/
 
 $(document).ready(function() {
-
-    const overlay = document.querySelector('#overlay')
     const viewMoreBtns = document.querySelectorAll('#viewMoreBtn')
+    // Add Overlay
+    const addOverlay = document.querySelector("#addOverlay");
+    const closeAddBtn = document.querySelector(".closeAddModal");
 
-    const toggleModal = () => {
-        overlay.classList.toggle('hidden')
-        overlay.classList.toggle('flex')
-    }
+    const toggleAddModal = () => {
+        addOverlay.classList.toggle("hidden");
+        addOverlay.classList.toggle("flex");
+    };
 
     const viewwMoreEventHandler = () => {
         console.log("View More Device");
@@ -31,4 +32,54 @@ $(document).ready(function() {
             document.postDeviceType.submit();
         })
     })
+
+
+    closeAddBtn.addEventListener("click", toggleAddModal);
+
+    $(document).on("click", "#cancelBtn", function() {
+        toggleAddModal();
+    });
+    $(document).on("click", "#addSubmitbtn", function() {
+        toggleAddModal();
+    });
+    $(document).on("click", "#addDeviceBtn", function() {
+        clickAdd(this);
+        toggleAddModal();
+    });
+
+    /***
+     * Display new type input when user choose others
+     */
+    $("#add-device-type").change(function() {
+        console.log("click");
+        var selectedValue = jQuery(this).val();
+        if (selectedValue === "Others") {
+            $(".newType").removeClass("hidden");
+        } else {
+            $(".newType").addClass("hidden");
+        }
+    });
 });
+
+function clickAdd(e) {
+    $.get("/Device/ViewAddDetails", function(data) {
+        console.log(data);
+        console.log("Last row Id: " + data.device.id);
+        document.getElementById("add-device-id").value = data.device.id + 1;
+        document.getElementById("add-labId").value = data.device.lab.labId
+        document.getElementById("add-labLocation").value = data.device.lab.labLocation
+        var deviceTypeDDL = document.getElementById("add-device-type")
+        if (deviceTypeDDL.length === 0) {
+            for (var i = 0; i < data.deviceTypes.length; i++) {
+                var element = document.createElement("option")
+                element.textContent = data.deviceTypes[i]
+                element.value = data.deviceTypes[i]
+                deviceTypeDDL.appendChild(element)
+            }
+            var last = document.createElement("option");
+            last.textContent = "Others";
+            last.value = "Others";
+            deviceTypeDDL.appendChild(last);
+        }
+    });
+}
