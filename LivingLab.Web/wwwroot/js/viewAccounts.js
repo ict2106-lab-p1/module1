02d766aadbbb92b569caa0d4569fcdbc84e9b996
bellesim <bellesimbeier@gmail.com>
2022-04-03 09:49:27 +0800

    fixed print problem, added cancel buttons

--- LivingLab.Web/wwwroot/js/viewAccounts.js
+++ LivingLab.Web/wwwroot/js/viewAccounts.js
@@ -3,8 +3,27 @@ $(document).ready(function () {
         clickEditUser(this)
     });
     $(document).on('click', '.deleteUserBtn', function () {
+
         clickDelete(this)
     });
+
+
+
+    // let closebtn2FA = document.getElementById("close-btn2FA");
+
+    $(document).on('click', '.modal-open', function () {
+        document.write("clicked")
+        let modal2FA = document.getElementById("modal-box");
+        modal2FA.style.display = "none";
+
+    });
+    let modal = document.getElementById("modal");
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 
     function clickEditUser(e) {
         $.get('/UserManagement/View/'+e.getAttribute('data-id'),  // url
@@ -23,4 +42,6 @@ $(document).ready(function () {
                 document.getElementById("del-user-email").value = data.email
             });
     }
+
+   
 })
